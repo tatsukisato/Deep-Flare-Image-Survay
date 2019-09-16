05b36358c920d156a5c07afb90454282a4375035
tatsukisato <tatsuki.10sato@gmail.com>
2019-09-16 19:51:13 +0900

    new file

---
+++ Data_Generation/make_fits.py
@@ -0,0 +1,34 @@
+#!/home/tsato/anaconda3/bin/python
+
+#
+#MAXI画像データ*rbn.img*から1orbitごとのimageをfits fileとして切り出す
+#fits/ディレクトリを生成し.fits形式で出力
+#
+
+def make_fits():
+    import os
+    import glob
+    import astropy.io.fits as pyfits
+    import shutil
+
+
+    PWD=os.getcwd()
+    new_dir_path = PWD + "/" +'fits'
+    FNAME=glob.glob('*rbn.img*')[0]
+    imgdata=pyfits.open(FNAME)[0].data
+    hpxdata=pyfits.open(FNAME)[1].data
+    if not os.path.exists(new_dir_path):
+        for i in range(imgdata.shape[0]):
+            start=hpxdata["START"][i]
+            stop=hpxdata["STOP"][i]
+            OUTFNAME="%03d_%d-%d.fits"%(i+1, int(start), int(stop))
+            img=imgdata[i]
+            hdu=pyfits.PrimaryHDU(img)
+            hdulist = pyfits.HDUList([hdu])
+            hdulist.writeto(OUTFNAME,overwrite=True)
+        os.mkdir(new_dir_path)
+        for i in glob.glob('./*.fits'):
+            shutil.move(i, "./fits/" )
+    else:
+        print ("fits Directory exist!!")
+make_fits()
